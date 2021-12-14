118402740bdec4776a48879141e6ead0c9a6fb32
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-12-14 23:09:19 +0500

    add bold class

--- FreeSpace-client/src/components/dashboard/CarrierPostDetailModal.js
+++ FreeSpace-client/src/components/dashboard/CarrierPostDetailModal.js
@@ -23,23 +23,24 @@ export default function DetailModal({post}) {
                 </Modal.Header>
                 <Modal.Body className='detail-post-body'>
                     <div>
-                        <span className='detail-post'>Departure Date: </span><span className='detail-post-answer'>{post.departureDate.slice(0,10)}</span>
+                        <span className='detail-post bold'>Departure Date: </span><span className='detail-post-answer'>{post.departureDate.slice(0,10)}</span>
                         <br></br><span className='detail-post'>Arrival Date: </span><span  className='detail-post-answer'>{post.arrivalDate.slice(0,10)}</span>
                     </div>
                     <div>
-                        <span className='detail-post'>Departure City: </span><span  className='detail-post-answer'>{post.departureCity}</span><br></br><span className='detail-post'>Arrival City: </span><span  className='detail-post-answer'>{post.arrivalCity}</span>
+                        <span className='detail-post bold'>Departure City: </span><span  className='detail-post-answer'>{post.departureCity}</span><br></br><span className='detail-post bold'>Arrival City: </span><span  className='detail-post-answer'>{post.arrivalCity}</span>
                     </div>
                     <div>
-                        <span className='detail-post'>Weight Allowance: </span><span  className='detail-post-answer'>{post.weight} kg(s)</span><br></br><span className='detail-post'>Volume: </span><span  className='detail-post-answer'>{post.volume}</span>
+                        <span className='detail-post bold'>Weight Allowance: </span><span  className='detail-post-answer'>{post.weight} kg(s)</span><br></br><span className='detail-post bold'>Volume: </span><span  className='detail-post-answer'>{post.volume}</span>
                     </div>
                     <div>
-                        <span className='detail-post'>Rates Per kg: </span><span  className='detail-post-answer'>{post.ratesPerKg}</span>
+                        <span className='detail-post bold'>Rates Per kg: </span><span  className='detail-post-answer'>{post.ratesPerKg}</span>
                     </div>
                     <div>
-                        <span className='detail-post'>Notes: </span><span  className='detail-post-answer'>{post.comments}</span>
+                        <span className='detail-post bold'>Notes: </span><span  className='detail-post-answer'>{post.comments}</span>
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
+                    <Card.Text>If you click INTERESTED button, a notification will be sent to the post owner with your email and phone numberthen you will be contacted soon, </Card.Text>
                     <Button className='livechat' onClick={
                         () => {
                             fetch(serverURL + `notification/create/`,
